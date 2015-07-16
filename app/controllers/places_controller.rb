837df1f87838fa9e0ef29aadc6ac64c8352f198b
kirubel <kirubel.aklilu@gmail.com>
2015-07-16 12:48:47 +0000

    Lesson 23 checkin

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,6 +15,21 @@ before_action :authenticate_user!, :only => [:new, :create]
 
 	end
 
+	def show
+	@place = Place.find(params[:id])	
+	end
+
+	def edit
+	@place = Place.find(params[:id])
+  	end
+
+  	def update
+  	@place = Place.find(params[:id])
+  	@place.update_attributes(place_params)
+  	redirect_to root_path
+  		
+  	end
+
 	private
 
 	def place_params
