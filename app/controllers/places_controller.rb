20fab14d66ce4a5ff1f6f4ac0ae36bfa524c6009
kirubel <kirubel.aklilu@gmail.com>
2015-07-17 12:02:22 +0000

    Lesson 27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,9 +10,12 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
-
+	@place = current_user.places.create(place_params)
+  	if @place.valid?
+    redirect_to root_path
+  	else
+    render :new, :status => :unprocessable_entity
+  	end
 	end
 
 	def show
@@ -32,6 +35,11 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
     return render :text => 'Not Allowed', :status => :forbidden
   	end
   	@place.update_attributes(place_params)
+  	 if @place.valid?
+    redirect_to root_path
+  	else
+    render :edit, :status => :unprocessable_entity
+  	end
   	redirect_to root_path
   		
   	end
