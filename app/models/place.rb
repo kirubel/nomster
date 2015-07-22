aeaae4a6ffcde2eb8514994f89a0f60d2e04e3d4
kirubel <kirubel.aklilu@gmail.com>
2015-07-22 02:06:14 +0000

    Geocoder working for at least some locations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-#	geocoded_by :address
-#  	after_validation :geocode
+	geocoded_by :address
+  	after_validation :geocode
 	validates :name, :presence => true
 end
