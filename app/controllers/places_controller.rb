886ac09ebe95451a2c4377713cd75c80e7603b90
kirubel <kirubel.aklilu@gmail.com>
2015-07-26 02:56:27 +0000

    Changed Pagination value to 5

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
 
     def index
     	# @places = Place.all
-      @places = Place.paginate(:page => params[:page], :per_page => 3)
+      @places = Place.paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def new
